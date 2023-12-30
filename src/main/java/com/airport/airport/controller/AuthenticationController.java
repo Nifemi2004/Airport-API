826ae3311d2ac60bd2e18777fc453047abb048d3
SokooyaN <sokoyanifemi@gmail.com>
2023-12-30 08:18:28 -0600

    Update AuthenticationController.java
    Made some minor changes to my Authentication Controller

--- src/main/java/com/airport/airport/controller/AuthenticationController.java
+++ src/main/java/com/airport/airport/controller/AuthenticationController.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
@@ -20,7 +20,7 @@ public class AuthenticationController {
     private final AuthenticationService service;
 
 
-        @PostMapping("/register")
+    @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody SignupDto signupDto
             ){
